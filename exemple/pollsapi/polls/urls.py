cde0b7e9ac996b7a2574845df8766517d3bb063f
Opeyemi19 <sodikfatayo@gmail.com>
2019-10-04 12:03:13 +0000

    first commit

--- exemple/pollsapi/polls/urls.py
+++ exemple/pollsapi/polls/urls.py
@@ -1,8 +1,29 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+
+# from .apiviews import PollList, PollDetail
+
+# urlpatterns = [
+#     path("polls/", PollList.as_view(), name="polls_list"),
+#     path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
+# ]
+
+# from .apiviews import *
+
+# urlpatterns = [
+#     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
+#     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
+
+# ]
+from rest_framework.routers import DefaultRouter
+from .apiviews import PollViewSet
+
+
+router = DefaultRouter()
+router.register('polls', PollViewSet, base_name='polls')
+
 
 urlpatterns = [
-    path('polls/', views.polls_list, name="polls_lists"),
-    path('polls/<int:pk>/', views.polls_detail, name="polls_detail"),
-
+    # ...
 ]
+
+urlpatterns += router.urls
