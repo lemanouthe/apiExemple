6de3f7d07c970bcf36e543e18008bf05b66c4a8e
Opeyemi19 <sodikfatayo@gmail.com>
2019-10-04 12:43:41 +0000

    secound

--- exemple/pollsapi/polls/apiviews.py
+++ exemple/pollsapi/polls/apiviews.py
@@ -1,40 +1,17 @@
-# from rest_framework import generics
-# from rest_framework.views import APIView
-# from rest_framework import status
-# from rest_framework.response import Response
-
-# from .models import Poll, Choice
-# from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
-
-# # ...
-# # PollList and PollDetail views
-
-# class ChoiceList(generics.ListCreateAPIView):
-#     def get_queryset(self):
-#         queryset = Choice.objects.filter(poll_id=self.kwargs["pk"])
-#         return queryset
-#     serializer_class = ChoiceSerializer
-
-
-# class CreateVote(APIView):
-#     serializer_class = VoteSerializer
-
-#     def post(self, request, pk, choice_pk):
-#         voted_by = request.data.get("voted_by")
-#         data = {'choice': choice_pk, 'poll': pk, 'voted_by': voted_by}
-#         serializer = VoteSerializer(data=data)
-#         if serializer.is_valid():
-#             vote = serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         else:
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 from rest_framework import viewsets
 
-from .models import Poll, Choice
+from .models import Poll, Choice, Vote
 from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
 
 
 class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
+    
+class ChoiceViewSet(viewsets.ModelViewSet):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+    
+class VoteViewSet(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
--- exemple/pollsapi/polls/urls.py
+++ exemple/pollsapi/polls/urls.py
@@ -15,11 +15,15 @@
 
 # ]
 from rest_framework.routers import DefaultRouter
-from .apiviews import PollViewSet
+from .apiviews import PollViewSet, ChoiceViewSet, VoteViewSet
 
 
 router = DefaultRouter()
 router.register('polls', PollViewSet, base_name='polls')
+router.register('choice', ChoiceViewSet, base_name='choices')
+router.register('vote', VoteViewSet, base_name='votes')
+
+
 
 
 urlpatterns = [
